587c8f30718f30bdd556cbb9df742577b06b101c
Michael Kang <michaelkang85@gmail.com>
2018-02-04 19:56:38 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,3 @@
-# def reverse_each_word(string)
-#   newArr = []
-#   arr = string.split(" ")
-#   arr.each |ele|
-#   newArr << ele.reverse!
-# end
-#   newArr.join(" ")
-# end
-#
-
-
-
 def reverse_each_word(string)
  newArr = []
  arr = string.split(" ")
